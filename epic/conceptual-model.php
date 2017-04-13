b95225dad04bb3457860a8ef1a637ca86d87abad
Jabari Farrar <jofarrar@gmail.com>
2017-04-13 11:28:12 -0600

    updated conceptual model and use case

--- epic/conceptual-model.php
+++ epic/conceptual-model.php
@@ -13,9 +13,25 @@
 			<header>
 
 				<h1>Conceptual Model</h1>
-
 			</header>
-
+			<p><strong>Profile</strong></p>
+			<ul>
+				<li>profileID (primary key)</li>
+				<li>profileActivationToken</li>
+				<li>profileEmail</li>
+				<li>profileHash</li>
+				<li>profilePhone</li>
+				<li>profileSalt</li>
+			</ul>
+			<br>
+			<p><strong>Favorite</strong></p>
+			<ul>
+				<li>likeProfileID</li>(foreign key)
+				<li>likeProductName</li>(foreign key)
+				<li>likeDate</li>
+			</ul>
+			<br>
+				<p><strong>Relations</strong></p>
 				<p>One verfied user can favorite many products one time</p>
 				<p>Many verified users can favorite many products one time </p>
 				<p>One verified user can unfavorite many products one time</p>
--- epic/use-case.php
+++ epic/use-case.php
@@ -17,15 +17,9 @@
 		</header>
 
 		<ul>
-			<li>User accesses website --> website opens</li>
-			<li>User searches products ---> system locates product under that search</li>
-			<li>User clicks on specific product --> system loads details for product</li>
-			<li>User clicks add-to-cart icon ---> System shows one item in cart</li>
-			<li>User clicks check out --> System prompts user to sign in</li>
-			<li>User signs in --> System assigns primary key to user and prompts for user's credit card info and shipping info</li>
-			<li>User recieves confiration e-mail from system</li>
-			<li>User returns to website after trying out product and logs in --> User is verified and can search again</li>
-			<li>User find purchased product and clicks thumbs-up --> System turns thumbs-up icon blue and prompts for product review</li>
+			<li>User logsin to website --> System welcomes ProfileID to website</li>
+			<li>User searches products ---> System locates product under that search</li>
+			<li>User favorites a product --> System stores favorites</li>
 		</ul>
 
 	</body>
